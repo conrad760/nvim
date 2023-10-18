ea3d65e7feb0c8ba59e30a644d472e1c47783ad3
Conrad Parker <conrad.parker@rewardstyle.com>
2023-10-17 22:25:21 -0600

    chore: adds auto to func

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -1,20 +1,24 @@
-local null_ls_status_ok, null_ls = pcall(require, "null-ls")
+local null_ls_status_ok, null_ls = pcall(require, "none-ls")
 if not null_ls_status_ok then
 	return
 end
 
--- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+-- https://github.com/jose-elias-alvarez/none-ls.nvim/tree/main/lua/null-ls/builtins/formatting
 local formatting = null_ls.builtins.formatting
--- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+-- https://github.com/jose-elias-alvarez/none-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
 
 null_ls.setup({
 	debug = false,
 	sources = {
-    require("null-ls").builtins.formatting.stylua,
-    require("null-ls").builtins.diagnostics.eslint,
-    require("null-ls").builtins.completion.spell,
---    require("null-ls").builtins.diagnostics.revive, -- this is great but I need a better config
+    require("none-ls").builtins.formatting.stylua,
+    require("none-ls").builtins.formatting.golines,
+    require("none-ls").builtins.formatting.gofumpt,
+    require("none-ls").builtins.formatting.goimports_reviser,
+
+    require("none-ls").builtins.diagnostics.eslint,
+    require("none-ls").builtins.completion.spell,
+--    require("none-ls").builtins.diagnostics.revive, -- this is great but I need a better config
 		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
 		formatting.stylua,
     diagnostics.flake8
