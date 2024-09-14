8d60ccda9cfbc80ff74bfd985084e615821a9cae
Conrad Parker <conradlparker@gmail.com>
2024-09-13 21:25:35 -0600

    chore: close close to open `c`

--- lua/plugins/vim-bbye.lua
+++ lua/plugins/vim-bbye.lua
@@ -1,6 +1,6 @@
 return {
 	"moll/vim-bbye",
 	config = function()
-		vim.keymap.set("n", "<leader>c", "<cmd>:Bdelete!<CR>", { desc = "close the current buffer" })
+		vim.keymap.set("n", "<leader>cc", "<cmd>:Bdelete!<CR>", { desc = "close the current buffer" })
 	end,
 }
