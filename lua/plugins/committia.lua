14320d77f494036567f546e04dd0f41e7a540988
Conrad Parker <conradlparker@gmail.com>
2024-09-22 20:15:27 -0600

    adds auto insert mode

--- lua/plugins/committia.lua
+++ lua/plugins/committia.lua
@@ -1,3 +1,27 @@
 return {
 	"rhysd/committia.vim", -- Git commit split diff screen
+	"rhysd/committia.vim",
+	ft = { "gitcommit", "gitsendemail", "*commit*", "*COMMIT*" },
+	dependencies = { "tpope/vim-fugitive" },
+	config = function()
+		vim.cmd([[
+                let g:committia_open_only_vim_starting = 1
+
+                let g:committia_hooks = {}
+                function! g:committia_hooks.edit_open(info)
+                " Additional settings
+                setlocal spell
+
+                " If no commit message, start with insert mode
+                if a:info.vcs ==# 'git' && getline(1) ==# ''
+                    startinsert
+                    endif
+
+                    " Scroll the diff window from insert mode
+                    " Map <C-n> and <C-p>
+                    imap <buffer><C-n> <Plug>(committia-scroll-diff-down-half)
+                    imap <buffer><C-p> <Plug>(committia-scroll-diff-up-half)
+                endfunction
+            ]])
+	end,
 }
--- lua/plugins/vim-fugitive.lua
+++ lua/plugins/vim-fugitive.lua
@@ -1,3 +1,8 @@
 return {
 	"tpope/vim-fugitive",
+	cmd = { "Git", "Gdiffsplit", "Gread", "Gwrite", "Ggrep", "GMove", "GDelete", "GBrowse" },
+	dependencies = { "tpope/vim-rhubarb" },
+	keys = {
+		{ "<leader>gB", ":GBrowse<cr>", mode = { "n", "v" }, desc = "[G]it [B]rowse" },
+	},
 }
