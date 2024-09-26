4bfb3e1873fdb148b640a02d101a9e670324439f
Conrad Parker <conradlparker@gmail.com>
2024-09-26 17:21:20 -0600

    new neogit

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,10 +1,14 @@
 return {
-	"TimUntersberger/neogit",
+	"NeogitOrg/neogit",
 	dependencies = {
 		"nvim-lua/plenary.nvim", -- required
 		"sindrets/diffview.nvim", -- optional - Diff integration
+
+		-- Only one of these is needed.
 		"nvim-telescope/telescope.nvim", -- optional
+		"ibhagwan/fzf-lua", -- optional
+		"echasnovski/mini.pick", -- optional
 	},
 	keys = { { "<leader>gg", "<cmd>:Neogit<CR>", desc = "Open Neogit" } },
-	opts = {},
+	config = true,
 }
