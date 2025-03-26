8530d1a22bb2045cec4ec92514076d0bc7a2e6af
Conrad L Parker <conradlparker@gmail.com>
2025-03-25 22:29:38 -0600

    I like which-key

--- lua/plugins/which-key_nvim.lua
+++ lua/plugins/which-key_nvim.lua
@@ -2,6 +2,6 @@ return { -- Useful plugin to show you pending keybinds.
 	"folke/which-key.nvim",
 	event = "VimEnter", -- Sets the loading event to 'VimEnter'
 	opts = {
-		delay = 1500,
+		delay = 200,
 	},
 }
