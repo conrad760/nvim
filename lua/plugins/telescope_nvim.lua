11ef260f572ed9b2b43c6327b099d042ca7e1981
Conrad L Parker <conradlparker@gmail.com>
2025-03-26 19:23:18 -0600

    adjust so I can still use / in other tools

--- lua/plugins/telescope_nvim.lua
+++ lua/plugins/telescope_nvim.lua
@@ -98,7 +98,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
 		-- )
 
 		-- Slightly advanced example of overriding default behavior and theme
-		vim.keymap.set("n", "/", function()
+		vim.keymap.set("n", "//", function()
 			-- You can pass additional configuration to Telescope to change the theme, layout, etc.
 			builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
 				winblend = 10,
