95c27a3e12ecfe41e006f44087fa8dfbed3de94c
Conrad L Parker <conradlparker@gmail.com>
2025-04-05 16:18:24 -0600

    snack changes

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -32,75 +32,15 @@ return {
 				end,
 				desc = "Git Log",
 			},
-			-- -- Iterate through incomplete tasks in Snacks_picker
-			-- {
-			-- 	-- -- You can confirm in your teminal lamw26wmal with:
-			-- 	-- -- rg "^\s*-\s\[ \]" test-markdown.md
-			-- 	"<leader>tt",
-			-- 	function()
-			-- 		Snacks.picker.grep({
-			-- 			prompt = " ",
-			-- 			-- pass your desired search as a static pattern
-			-- 			search = "^\\s*- \\[ \\]",
-			-- 			-- we enable regex so the pattern is interpreted as a regex
-			-- 			regex = true,
-			-- 			-- no “live grep” needed here since we have a fixed pattern
-			-- 			live = false,
-			-- 			-- restrict search to the current working directory
-			-- 			dirs = { vim.fn.getcwd() },
-			-- 			-- include files ignored by .gitignore
-			-- 			args = { "--no-ignore" },
-			-- 			-- Start in normal mode
-			-- 			on_show = function()
-			-- 				vim.cmd.stopinsert()
-			-- 			end,
-			-- 			finder = "grep",
-			-- 			format = "file",
-			-- 			show_empty = true,
-			-- 			supports_live = false,
-			-- 			layout = "ivy",
-			-- 		})
-			-- 	end,
-			-- 	desc = "[P]Search for incomplete tasks",
-			-- },
-			-- -- Iterate throuth completed tasks in Snacks_picker lamw26wmal
-			-- {
-			-- 	"<leader>tc",
-			-- 	function()
-			-- 		Snacks.picker.grep({
-			-- 			prompt = " ",
-			-- 			-- pass your desired search as a static pattern
-			-- 			search = "^\\s*- \\[x\\] `done:",
-			-- 			-- we enable regex so the pattern is interpreted as a regex
-			-- 			regex = true,
-			-- 			-- no “live grep” needed here since we have a fixed pattern
-			-- 			live = false,
-			-- 			-- restrict search to the current working directory
-			-- 			dirs = { vim.fn.getcwd() },
-			-- 			-- include files ignored by .gitignore
-			-- 			args = { "--no-ignore" },
-			-- 			-- Start in normal mode
-			-- 			on_show = function()
-			-- 				vim.cmd.stopinsert()
-			-- 			end,
-			-- 			finder = "grep",
-			-- 			format = "file",
-			-- 			show_empty = true,
-			-- 			supports_live = false,
-			-- 			layout = "ivy",
-			-- 		})
-			-- 	end,
-			-- 	desc = "[P]Search for complete tasks",
-			-- },
 			-- -- List git branches with Snacks_picker to quickly switch to a new branch
 			{
-				"<M-b>",
+				"<M-l>",
 				function()
-					Snacks.picker.git_branches({
-						layout = "select",
+					Snacks.picker.git_log({
+						layout = "telescope",
 					})
 				end,
-				desc = "Branches",
+				desc = "Logs",
 			},
 			-- Used in LazyVim to view the different keymaps, this by default is
 			-- configured as <leader>sk but I run it too often
@@ -123,8 +63,6 @@ return {
 						format = "file",
 						show_empty = true,
 						supports_live = true,
-						-- In case you want to override the layout for this keymap
-						-- layout = "vscode",
 					})
 				end,
 				desc = "Find Files",
@@ -167,20 +105,20 @@ return {
 				-- file was always showing at the top, I needed a way to decrease its
 				-- score, in frecency you could use :FrecencyDelete to delete a file
 				-- from the database, here you can decrease it's score
-				transform = function(item)
-					if not item.file then
-						return item
-					end
-					-- Demote the "lazyvim" keymaps file:
-					if item.file:match("lazyvim/lua/config/keymaps%.lua") then
-						item.score_add = (item.score_add or 0) - 30
-					end
-					-- Boost the "neobean" keymaps file:
-					-- if item.file:match("neobean/lua/config/keymaps%.lua") then
-					--   item.score_add = (item.score_add or 0) + 100
-					-- end
-					return item
-				end,
+				-- transform = function(item)
+				-- 	if not item.file then
+				-- 		return item
+				-- 	end
+				-- 	-- Demote the "lazyvim" keymaps file:
+				-- 	if item.file:match("lazyvim/lua/config/keymaps%.lua") then
+				-- 		item.score_add = (item.score_add or 0) - 30
+				-- 	end
+				-- 	-- Boost the "neobean" keymaps file:
+				-- 	-- if item.file:match("neobean/lua/config/keymaps%.lua") then
+				-- 	--   item.score_add = (item.score_add or 0) + 100
+				-- 	-- end
+				-- 	return item
+				-- end,
 				-- In case you want to make sure that the score manipulation above works
 				-- or if you want to check the score of each file
 				debug = {
@@ -222,7 +160,6 @@ return {
 						},
 					},
 					-- 		-- I wanted to modify the layout width
-					-- 		--
 					vertical = {
 						layout = {
 							backdrop = false,
@@ -240,9 +177,9 @@ return {
 						},
 					},
 				},
-				matcher = {
-					frecency = true,
-				},
+				-- matcher = {
+				-- 	frecency = true,
+				-- },
 				win = {
 					input = {
 						keys = {
@@ -264,29 +201,6 @@ return {
 					},
 				},
 			},
-			-- Folke pointed me to the snacks docs
-			-- https://github.com/LazyVim/LazyVim/discussions/4251#discussioncomment-11198069
-			-- Here's the lazygit snak docs
-			-- https://github.com/folke/snacks.nvim/blob/main/docs/lazygit.md
-			-- lazygit = {
-			-- 	theme = {
-			-- 		selectedLineBgColor = { bg = "CursorLine" },
-			-- 	},
-			-- 	-- With this I make lazygit to use the entire screen, because by default there's
-			-- 	-- "padding" added around the sides
-			-- 	-- I asked in LazyGit, folke didn't like it xD xD xD
-			-- 	-- https://github.com/folke/snacks.nvim/issues/719
-			-- 	win = {
-			-- 		-- -- The first option was to use the "dashboard" style, which uses a
-			-- 		-- -- 0 height and width, see the styles documentation
-			-- 		-- -- https://github.com/folke/snacks.nvim/blob/main/docs/styles.md
-			-- 		-- style = "dashboard",
-			-- 		-- But I can also explicitly set them, which also works, what the best
-			-- 		-- way is? Who knows, but it works
-			-- 		width = 0,
-			-- 		height = 0,
-			-- 	},
-			-- },
 			notifier = {
 				enabled = false,
 				top_down = false, -- place notifications from top to bottom
@@ -298,64 +212,6 @@ return {
 				snacks_image = {
 					relative = "editor",
 					col = -1,
-				},
-			},
-			image = {
-				enabled = true,
-				doc = {
-					-- Personally I set this to false, I don't want to render all the
-					-- images in the file, only when I hover over them
-					-- render the image inline in the buffer
-					-- if your env doesn't support unicode placeholders, this will be disabled
-					-- takes precedence over `opts.float` on supported terminals
-					inline = vim.g.neovim_mode == "skitty" and true or false,
-					-- only_render_image_at_cursor = vim.g.neovim_mode == "skitty" and false or true,
-					-- render the image in a floating window
-					-- only used if `opts.inline` is disabled
-					float = true,
-					-- Sets the size of the image
-					-- max_width = 60,
-					max_width = vim.g.neovim_mode == "skitty" and 20 or 60,
-					max_height = vim.g.neovim_mode == "skitty" and 10 or 30,
-					-- max_height = 30,
-					-- Apparently, all the images that you preview in neovim are converted
-					-- to .png and they're cached, original image remains the same, but
-					-- the preview you see is a png converted version of that image
-					--
-					-- Where are the cached images stored?
-					-- This path is found in the docs
-					-- :lua print(vim.fn.stdpath("cache") .. "/snacks/image")
-					-- For me returns `~/.cache/neobean/snacks/image`
-					-- Go 1 dir above and check `sudo du -sh ./* | sort -hr | head -n 5`
-				},
-			},
-			dashboard = {
-				preset = {
-					keys = {
-						-- { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-						-- { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-						-- { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
-						-- { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
-						-- {
-						--   icon = " ",
-						--   key = "c",
-						--   desc = "Config",
-						--   action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
-						-- },
-						{ icon = " ", key = "s", desc = "Restore Session", section = "session" },
-						-- { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
-						{ icon = " ", key = "<esc>", desc = "Quit", action = ":qa" },
-					},
-					header = [[
-███╗   ██╗███████╗ ██████╗ ██████╗ ███████╗ █████╗ ███╗   ██╗
-████╗  ██║██╔════╝██╔═══██╗██╔══██╗██╔════╝██╔══██╗████╗  ██║
-██╔██╗ ██║█████╗  ██║   ██║██████╔╝█████╗  ███████║██╔██╗ ██║
-██║╚██╗██║██╔══╝  ██║   ██║██╔══██╗██╔══╝  ██╔══██║██║╚██╗██║
-██║ ╚████║███████╗╚██████╔╝██████╔╝███████╗██║  ██║██║ ╚████║
-╚═╝  ╚═══╝╚══════╝ ╚═════╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝
-
-[Linkarzu.com]
-        ]],
 				},
 			},
 		},
