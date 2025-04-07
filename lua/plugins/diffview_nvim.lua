6af3fd7b6462a3a048a57348e0b913265223657a
Conrad L Parker <ConradLParker@gmail.com>
2025-04-07 07:18:11 -0600

    feat(tools): diffview movements
    Adds some additional mappings to the diffview to make it easier to navigate.

--- lua/plugins/diffview_nvim.lua
+++ lua/plugins/diffview_nvim.lua
@@ -1,3 +1,14 @@
 return {
 	"sindrets/diffview.nvim",
+	cmd = { "DiffviewOpen", "DiffviewFileHistory" },
+	keys = {
+		{ "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Open DiffView" },
+		{ "<leader>gD", "<cmd>DiffviewOpen origin/main...HEAD<cr>", desc = "Open DiffView (against main)" },
+		{ "<leader>gh", "<cmd>DiffviewFileHistory<cr>", desc = "File history (repo)" },
+		{ "<leader>gH", "<cmd>DiffviewFileHistory %<cr>", desc = "File history (file)" },
+		{ "<leader>gq", "<cmd>DiffviewClose<cr>", desc = "Close DiffView" },
+	},
+	opts = {
+		use_icons = true,
+	},
 }
