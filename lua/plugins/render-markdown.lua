a8eb200a593d6618745d63d52b34e074d2061941
Conrad L Parker <ConradLParker@gmail.com>
2025-04-22 00:57:14 -0600

    style(colors): inline code block colors

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -22,8 +22,8 @@ return {
 	-- :checkhealth render-markdown
 	-- https://github.com/MeanderingProgrammer/render-markdown.nvim/issues/138#issuecomment-2295422741
 	init = function()
-		local colorInline_bg = colors["linkarzu_color02"]
-		local color_fg = colors["linkarzu_color26"]
+		local colorInline_bg = "#ebfafa" -- colors["linkarzu_color02"]
+		-- local color_fg = colors["linkarzu_color26"]
 		-- local color_sign = "#ebfafa"
 		-- Define color variables
 		local color1_bg = colors["linkarzu_color04"]
@@ -47,8 +47,8 @@ return {
 		vim.cmd(string.format([[highlight Headline5Bg guibg=%s guifg=%s ]], color_fg5, color5_bg))
 		vim.cmd(string.format([[highlight Headline6Bg guibg=%s guifg=%s ]], color_fg6, color6_bg))
 		-- Define inline code highlight for markdown
-		vim.cmd(string.format([[highlight RenderMarkdownCodeInline guifg=%s guibg=%s]], colorInline_bg, color_fg))
-		-- vim.cmd(string.format([[highlight RenderMarkdownCodeInline guifg=%s]], colorInline_bg))
+		-- vim.cmd(string.format([[highlight RenderMarkdownCodeInline guifg=%s guibg=%s]], colorInline_bg, color_fg))
+		vim.cmd(string.format([[highlight RenderMarkdownCodeInline guifg=%s]], colorInline_bg))
 
 		-- Highlight for the heading and sign icons (symbol on the left)
 		-- I have the sign disabled for now, so this makes no effect
@@ -60,6 +60,78 @@ return {
 		vim.cmd(string.format([[highlight Headline6Fg cterm=bold gui=bold guifg=%s]], color6_bg))
 	end,
 	opts = {
+		code = {
+			-- Turn on / off code block & inline code rendering.
+			enabled = true,
+			-- Additional modes to render code blocks.
+			render_modes = false,
+			-- Turn on / off any sign column related rendering.
+			sign = true,
+			-- Determines how code blocks & inline code are rendered.
+			-- | none     | disables all rendering                                                    |
+			-- | normal   | highlight group to code blocks & inline code, adds padding to code blocks |
+			-- | language | language icon to sign column if enabled and icon + name above code blocks |
+			-- | full     | normal + language                                                         |
+			style = "full",
+			-- Determines where language icon is rendered.
+			-- | right | right side of code block |
+			-- | left  | left side of code block  |
+			position = "left",
+			-- Amount of padding to add around the language.
+			-- If a float < 1 is provided it is treated as a percentage of available window space.
+			language_pad = 0,
+			-- Whether to include the language icon above code blocks.
+			language_icon = false,
+			-- Whether to include the language name above code blocks.
+			language_name = false,
+			-- A list of language names for which background highlighting will be disabled.
+			-- Likely because that language has background highlights itself.
+			-- Use a boolean to make behavior apply to all languages.
+			-- Borders above & below blocks will continue to be rendered.
+			disable_background = { "diff" },
+			-- Width of the code block background.
+			-- | block | width of the code block  |
+			-- | full  | full width of the window |
+			width = "block",
+			-- Amount of margin to add to the left of code blocks.
+			-- If a float < 1 is provided it is treated as a percentage of available window space.
+			-- Margin available space is computed after accounting for padding.
+			left_margin = 1,
+			-- Amount of padding to add to the left of code blocks.
+			-- If a float < 1 is provided it is treated as a percentage of available window space.
+			left_pad = 0,
+			-- Amount of padding to add to the right of code blocks when width is 'block'.
+			-- If a float < 1 is provided it is treated as a percentage of available window space.
+			right_pad = 2,
+			-- Minimum width to use for code blocks when width is 'block'.
+			min_width = 80,
+			-- Determines how the top / bottom of code block are rendered.
+			-- | none  | do not render a border                               |
+			-- | thick | use the same highlight as the code body              |
+			-- | thin  | when lines are empty overlay the above & below icons |
+			-- | hide  | conceal lines unless language name or icon is added  |
+			border = "hide",
+			-- Used above code blocks for thin border.
+			above = "▁",
+			-- Used below code blocks for thin border.
+			below = "▔",
+			-- Icon to add to the left of inline code.
+			inline_left = "",
+			-- Icon to add to the right of inline code.
+			inline_right = "",
+			-- Padding to add to the left & right of inline code.
+			inline_pad = 0,
+			-- Highlight for code blocks.
+			highlight = "RenderMarkdownCode",
+			-- Highlight for language, overrides icon provider value.
+			highlight_language = nil,
+			-- Highlight for border, use false to add no highlight.
+			highlight_border = "RenderMarkdownCodeBorder",
+			-- Highlight for language, used if icon provider does not have a value.
+			highlight_fallback = "RenderMarkdownCodeFallback",
+			-- Highlight for inline code.
+			highlight_inline = "RenderMarkdownCodeInline",
+		},
 		bullet = {
 			-- Turn on / off list bullet rendering
 			enabled = true,
